2ceb399f6fd9532982dfe1f0c7547aa4242569da
이은영 <110472265+Eunyomi@users.noreply.github.com>
2022-10-20 17:41:52 +0900

    Create MyApp005.cs

---
+++ MyApp005.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = 'A';
+            char ch1 = '\t';
+            char ch2 = '\n';
+            char ch3 = 'o';
+
+            string str1 = "\tABC\n";
+
+            Console.WriteLine(ch);
+            Console.WriteLine(ch1);
+            Console.WriteLine(ch2);
+            Console.WriteLine(ch3); 
+        
+            Console.WriteLine(str1);
+        }
+    }
+}
