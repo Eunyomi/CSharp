24a39aa4cbbc73d130a0d8e6be75f92b333f69f8
이은영 <110472265+Eunyomi@users.noreply.github.com>
2022-10-20 14:36:53 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp002
+{
+    class MyApp
+    {   /* 프로그램 운명 설계도 */
+        // 멤버변수
+        // 생성자
+        // 멤버함수
+        static void Main(string[] args)
+        {
+            /* 시간이 흐르는 Real Time */
+            Console.WriteLine("안녕하세요. C#");
+
+        }
+    }
+}
